a73852d7a3be34ad63b09e10180339730cec7d0f
Alex Grozav <alex@grozav.com>
2024-01-31 14:08:12 +0200

    feat: change to using runtime env variable for preview mode

--- packages/workflow/src/Interfaces.ts
+++ packages/workflow/src/Interfaces.ts
@@ -2384,6 +2384,7 @@ export interface IN8nUISettings {
 	workflowTagsDisabled: boolean;
 	logLevel: LogLevel;
 	hiringBannerEnabled: boolean;
+	previewModeEnabled: boolean;
 	templates: {
 		enabled: boolean;
 		host: string;
